431d63d9ec2d272eed529071e4b8cf62dde9dafa
GATtomonyan415 <70007651+GATtomonyan415@users.noreply.github.com>
2022-01-31 11:17:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,14 +11,15 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@bot.event
-async def on_ready():
-await bot.change_presence(activity=discord.Game(f"#この川、深いッ！")
+
     
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
+    @bot.event
+async def on_ready():
+await bot.change_presence(activity=discord.Game(f"#この川、深いッ！")
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
