d2a845dabcf761f0ed8887d064daf702af7876b3
GATtomonyan415 <70007651+GATtomonyan415@users.noreply.github.com>
2022-02-20 11:46:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,14 @@
 from discord.ext import commands
 from os import getenv
 import traceback
+import discord
+from discord.ext import commands
+#pip install discord-buttons-plugin
 from discord_buttons_plugin import *
-buttons = ButtonsClient(bot)
+#pip install requests
 import requests
-
-bot = commands.Bot(command_prefix='b!')
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+bot = commands.Bot(command_prefix = "b!")
+buttons = ButtonsClient(bot)
 
 @bot.event
 async def on_ready():
@@ -52,7 +43,5 @@ async def create(ctx):
 			])
 		]
 	)
-
-    
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
