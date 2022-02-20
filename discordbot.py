6e6865c04da42f35649cb5f41b16ef224906c897
GATtomonyan415 <70007651+GATtomonyan415@users.noreply.github.com>
2022-02-20 11:40:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 from os import getenv
 import traceback
+from discord_buttons_plugin import *
+buttons = ButtonsClient(bot)
 
 bot = commands.Bot(command_prefix='b!')
 
@@ -16,6 +18,40 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.event
+async def on_ready():
+	print("準備完了")
+  
+@buttons.click
+async def button_hello(ctx):
+	await ctx.reply("こんにちは！")
 
+@buttons.click
+async def button_ephemeral(ctx):
+	await ctx.reply("このメッセージはあなたにしか見えていません！", flags = MessageFlags().EPHEMERAL)
+
+@bot.command()
+async def create(ctx):
+	await buttons.send(
+		content = "テストボタン", 
+		channel = ctx.channel.id,
+		components = [
+			ActionRow([
+				Button(
+					label="Hello", 
+					style=ButtonType().Primary, 
+					custom_id="button_hello"
+				)
+			]),ActionRow([
+				Button(
+					label="Ephemeral",
+					style=ButtonType().Danger,
+					custom_id="button_ephemeral"
+				)
+			])
+		]
+	)
+
+    
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
